6cc629014a9bd5ba93f16294720611f84d0f8985
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2017-04-25 11:52:18 +0200

    add SAGE and various font configs for glossary

--- Glossary/read_and_write.py
+++ Glossary/read_and_write.py
@@ -10,11 +10,12 @@ output.write("\\documentclass[landscape]{article}\n\n")
 output.write("\\usepackage{array}\n")
 output.write("\\usepackage{url}\n")
 output.write("\\usepackage{graphicx}\n")
-output.write("\\usepackage[hmargin=0.6cm,vmargin=1.3cm]{geometry}\n")
-output.write("\\usepackage{lmodern}\n")
+output.write("\\usepackage[hmargin=0.5cm,vmargin=1.3cm]{geometry}\n")
+output.write("\\usepackage{stix}\n")
 # output.write("\\usepackage{avant}\n")
 output.write("\\setlength{\\extrarowheight}{.5pt}\n")
 output.write("\\newcommand{\\CC}{C\\nolinebreak\\hspace{-.05em}\\raisebox{.4ex}{\\tiny\\bf +}\\nolinebreak\\hspace{-.10em}\\raisebox{.4ex}{\\tiny\\bf +}}")
+output.write("\\renewcommand*{\\UrlFont}{\\ttfamily\\small\\relax}")
 
 output.write("\\begin{document}\n")
 output.write("\\thispagestyle{empty}\n")
@@ -25,30 +26,33 @@ l2 = l[len(l)//2:]
 for l in (l1,l2):
     output.write("\\begin{minipage}{.5\\textwidth}\n")
     if l is l1:
-        output.write("\\begin{minipage}{.4\\textwidth}{\\centering \\Huge OpenDreamKit \\ Glossary}\\end{minipage}\n")
-        output.write("\\hspace{.1\\textwidth}")
-        output.write("\\begin{minipage}{.1\\textwidth}\\includegraphics[scale=0.15]{logos/odk.png}\\end{minipage}\n")
-        output.write("\\hspace{.1\\textwidth}")
-        output.write("\\begin{minipage}{.1\\textwidth}\\includegraphics[scale=0.1]{logos/europe.png}\\end{minipage}\n")
+        output.write("\\begin{minipage}{.65\\textwidth}{\\Huge OpenDreamKit Glossary}\end{minipage}")
+        output.write("\\hspace{.03\\textwidth}")
+        output.write("\\begin{minipage}{.3\\textwidth}")
+        output.write("\\includegraphics[scale=0.12]{logos/odk.png}")
+        output.write("\\hspace{.18\\textwidth}")
+        output.write("\\includegraphics[scale=0.06]{logos/europe.png}")
+        output.write("\\end{minipage}")
         output.write("\\vspace{.7cm}\n\n")
 
-    output.write("\\noindent \\begin{tabular}{m{.2\\textwidth}p{.7\\textwidth}}\n")
+    output.write("\\noindent \\begin{tabular}{m{.15\\textwidth}p{.7\\textwidth}}\n")
     output.write("\\hline\n")
     for dat in l:
+        name = dat['acronym'] if 'acronym' in dat else dat['name']
+
         if 'logo' in dat:
             scale = '[scale={}]'.format(dat['logo-rescale']) if 'logo-rescale' in dat else ''
 
             if dat.get('force-name'):
-                output.write('\\begin{minipage}{.2\\textwidth}\n')
-                output.write('{{\\large \\textbf{{{}}}}}\n\\\\'.format(dat['name']))
+                output.write('\\begin{minipage}{.15\\textwidth}\n')
+                output.write('{{\\large {}}}\n\\\\'.format(name))
                 output.write("\includegraphics{}{{logos/{}}}\n".format(scale, dat['logo']))
                 output.write('\\end{minipage}&\n')
             else:
                 output.write("\includegraphics{}{{logos/{}}} & \n".format(scale, dat['logo']))
 
         else:
-            name = dat['acronym'] if 'acronym' in dat else dat['name']
-            output.write("{{\large \\textbf{{{}}}}} & \n".format(name))
+            output.write("{{\\large {}}} & \n".format(name))
 
         desc = dat.get('description', '')
         if 'acronym' in dat:
